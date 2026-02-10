9c1be35fa4ee7543801635022ab939fab30a84ec
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-10 17:18:18 +0100

    Add AccountManager same features as UserManager

--- server/utils/AccountManager.ts
+++ server/utils/AccountManager.ts
@@ -1,18 +1,46 @@
-// TODO: Add Functionally
+import { db } from '../database/client';
+import { account } from '../database/schema/account';
+import { eq } from 'drizzle-orm';
 
 class AccountManager {
      // Delete Functions
-     public deletebyID() {}
+     public async deletebyID(id: string) {
+          await db.delete(account).where(eq(account.id, id));
+     }
      public deleteMany() {}
-     public deleteAll() {}
+     public async deleteAll() {
+          await db.delete(account);
+     }
 
      // Create Functions
-     public create() {}
+     public async create(
+          owner_id: string,
+          title: string,
+          description?: string,
+          created_at?: Date,
+     ) {
+          await db.insert(account).values({
+               owner_id: owner_id,
+               title: title,
+               description: description,
+               created_at: created_at,
+          });
+     }
      public createMany() {}
 
      // Get Functions
-     public getbyID() {}
-     public getAll() {}
+     public async getbyID(id: string) {
+          const result = await db
+               .select()
+               .from(account)
+               .where(eq(account.id, id))
+               .limit(1);
+          return result[0];
+     }
+     public async getAll() {
+          const result = await db.select().from(account);
+          return result;
+     }
 
      // Modify Functions
      public modifybyID() {}
