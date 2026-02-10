0c3b4d76994f6c3991de9137f39601f722551329
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-10 16:48:19 +0100

    Add Create, deletebyID, getbyID, GetbyUsername, GetbyEmail, GetbyEmailOrUsername and Getall functions to UserManager

--- server/utils/UserManager.ts
+++ server/utils/UserManager.ts
@@ -1,23 +1,80 @@
-// TODO: Add Functionally
+import { db } from '../database/client';
+import { user } from '../database/schema/user';
+import { eq, or } from 'drizzle-orm';
 
 class UserManager {
      // Delete Functions
-     public deletebyID() {}
+     public async deletebyID(id: string) {
+          await db.delete(user).where(eq(user.id, id));
+     }
      public deleteMany() {}
      public deleteAll() {}
 
      // Create Functions
-     public create() {}
-     public createMany() {}
+     public async create(
+          username: string,
+          displayname: string,
+          hashed_password: string,
+          email?: string,
+          created_at?: Date,
+     ) {
+          await db.insert(user).values({
+               username: username,
+               displayname: displayname,
+               password: hashed_password,
+               email: email,
+               created_at: created_at,
+          });
+     }
+     public async createMany() {}
 
      // Get Functions
-     public getbyID() {}
-     public getbyUsername() {}
-     public getbyEmail() {}
-     public getbyEmailOrUsername() {}
-     public getAll() {}
+     public async getbyID(id: string) {
+          const result = await db
+               .select()
+               .from(user)
+               .where(eq(user.id, id))
+               .limit(1);
+          return result[0];
+     }
+     public async getManybyIDs() {}
+     public async getbyUsername(username: string) {
+          const result = await db
+               .select()
+               .from(user)
+               .where(eq(user.username, username))
+               .limit(1);
+          return result[0];
+     }
+     public async getbyEmail(email: string) {
+          const result = await db
+               .select()
+               .from(user)
+               .where(eq(user.email, email))
+               .limit(1);
+          return result[0];
+     }
+     public async getbyEmailOrUsername(username_or_email: string) {
+          const result = await db
+               .select()
+               .from(user)
+               .where(
+                    or(
+                         eq(user.email, username_or_email),
+                         eq(user.username, username_or_email),
+                    ),
+               )
+               .limit(1);
+          return result[0];
+     }
+     public async getAll() {
+          const result = await db.select().from(user);
+          return result;
+     }
 
      // Modify Functions
-     public modifybyID() {}
-     public modifybyMany() {}
+     public async modifybyID() {}
+     public async modifybyMany() {}
 }
+
+export const userManager = new UserManager();
