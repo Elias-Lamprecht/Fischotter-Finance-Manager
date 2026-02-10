ad0ad1b2149cb9ed01f49581ff3f26ab8fca426a
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-10 16:50:45 +0100

    make user email optional

--- server/database/schema/user.ts
+++ server/database/schema/user.ts
@@ -2,7 +2,7 @@ import { pgTable, uuid, varchar, timestamp } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('users', {
      id: uuid('id').defaultRandom().primaryKey(),
-     email: varchar('email', { length: 32 }).notNull().unique(),
+     email: varchar('email', { length: 32 }).unique(),
      username: varchar('username', { length: 32 }).notNull().unique(),
      displayname: varchar('displayname', { length: 32 }).notNull(),
      password: varchar('password', { length: 64 }).notNull(),
