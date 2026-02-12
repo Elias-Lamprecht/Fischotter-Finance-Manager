e4f8a13e4957629ee4c02179939dba5ade3734cc
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-12 12:51:03 +0100

    API Addition: api/management/modify/user

---
+++ server/api/management/modify/user.post.ts
@@ -0,0 +1,36 @@
+import { getFullAuthCookieContent } from '../../../utils/getFullAuthCookieContent';
+import { db } from '../../../database/client';
+import { user } from '../../../database/schema/user';
+import { eq } from 'drizzle-orm';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+     const body = await readBody(event);
+
+     // TODO: Add the generalized Error Messages
+     if (FullAuthCookieContent === null) {
+          return { success: false, message: "User isn't logged in" };
+     }
+
+     // TODO: Add the generalized Error Messages
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { success: false, message: "User isn't a adminstrator" };
+     }
+     try {
+          await db
+               .update(user)
+               .set({
+                    username: body.username,
+                    displayname: body.displayname,
+                    role: body.role,
+                    email: body.email,
+               })
+               .where(eq(user.id, body.id));
+          console.log('returned true!');
+
+          return { success: true };
+     } catch (error: any) {
+          console.log('Register API Error:', error);
+          return { success: false, error: error?.message ?? error };
+     }
+});
