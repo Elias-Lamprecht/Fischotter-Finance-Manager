61026b921c5dca1ce8227ef11c01161ce54ebe48
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 23:07:29 +0100

    change - schema: update transaction schema to use price instead of value

--- server/database/schema/transaction.ts
+++ server/database/schema/transaction.ts
@@ -1,18 +1,13 @@
-import {
-     pgTable,
-     uuid,
-     varchar,
-     timestamp,
-     numeric,
-} from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, timestamp, numeric } from 'drizzle-orm/pg-core';
 
 export const transaction = pgTable('transactions', {
      id: uuid('id').defaultRandom().primaryKey(),
      owner_id: uuid('owner_id').notNull(),
      account_id: uuid('account_id').notNull(),
+     subscription_id: uuid('subscription_id'),
      title: varchar('title', { length: 32 }).notNull(),
      description: varchar('description', { length: 128 }),
      type: varchar('type', { length: 16 }).notNull(),
-     value: numeric('value', { precision: 12, scale: 2 }).notNull(),
+     price: numeric('price', { precision: 12, scale: 2 }).notNull(),
      created_at: timestamp('created_at').defaultNow(),
 });
