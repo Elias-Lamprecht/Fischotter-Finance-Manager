23f8c8180c29deb1e6bdc66c0058cde291393246
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 19:37:12 +0100

    bug fix: Add check if id is present in request. (api/management/delete/by-id/user)

--- server/api/management/delete/by-id/user.delete.ts
+++ server/api/management/delete/by-id/user.delete.ts
@@ -7,6 +7,10 @@ export default defineEventHandler(async (event) => {
      const FullAuthCookieContent = getFullAuthCookieContent(event);
      const body = await readBody(event);
 
+     if (!body.id) {
+          return { success: false, message: 'Missing ID' };
+     }
+
      // TODO: Add the generalized Error Messages
      if (FullAuthCookieContent === null) {
           return { success: false, message: "User isn't logged in" };
