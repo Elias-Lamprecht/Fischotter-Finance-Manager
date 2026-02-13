359641f26fdbeb9decc3fee0adcf213ccd095542
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 23:17:10 +0100

    feat - API: add Create & Modify API's for transactions.

---
+++ server/api/management/create/transaction.post.ts
@@ -0,0 +1,47 @@
+import { getFullAuthCookieContent } from '../../../utils/getFullAuthCookieContent';
+import { db } from '../../../database/client';
+import { transaction } from '../../../database/schema/transaction';
+import { ERRORS } from '~~/server/utils/errors';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+     const body = await readBody(event);
+
+     console.log('Missing fields:', {
+          owner_id: !body.owner_id,
+          account_id: !body.account_id,
+          title: !body.title,
+          type: !body.type,
+          price: !body.price,
+     });
+
+     if (!body.owner_id || !body.account_id || !body.title || !body.type || !body.price) {
+          return { state: 'error', message: ERRORS.GENERAL.MISSING_DATA };
+     }
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { state: 'denied', message: ERRORS.AUTH.INSUFFICIENT_PERMISSIONS };
+     }
+
+     try {
+          const result = await db
+               .insert(transaction)
+               .values({
+                    account_id: body.account_id,
+                    owner_id: body.owner_id,
+                    title: body.title,
+                    description: body.description,
+                    type: body.type,
+                    price: body.price,
+               })
+               .returning();
+
+          return { state: 'success', data: result };
+     } catch (error: any) {
+          console.log('Create Transaction API Error:', error);
+     }
+});
---
+++ server/api/management/modify/transaction.post.ts
@@ -0,0 +1,61 @@
+import { getFullAuthCookieContent } from '../../../utils/getFullAuthCookieContent';
+import { db } from '../../../database/client';
+import { transaction } from '../../../database/schema/transaction';
+import { eq } from 'drizzle-orm';
+import { ERRORS } from '~~/server/utils/errors';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+     const body = await readBody(event);
+
+     if (
+          !body.id &&
+          !body.account_id &&
+          !body.owner_id &&
+          !body.title &&
+          !body.description &&
+          !body.type
+     ) {
+          return {
+               state: 'error',
+               message: ERRORS.UPDATE.NO_CHANGES_DETECTED,
+          };
+     }
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { state: 'denied', message: ERRORS.AUTH.INSUFFICIENT_PERMISSIONS };
+     }
+
+     try {
+          console.log('updating transaction.');
+          console.log({
+               id: body.id,
+               account_id: body.account_id,
+               owner_id: body.owner_id,
+               title: body.title,
+               description: body.description,
+               type: body.type,
+               price: body.price,
+          });
+          await db
+               .update(transaction)
+               .set({
+                    id: body.id,
+                    account_id: body.account_id,
+                    owner_id: body.owner_id,
+                    title: body.title,
+                    description: body.description,
+                    type: body.type,
+                    price: body.price,
+               })
+               .where(eq(transaction.id, body.id));
+
+          return { state: 'success' };
+     } catch (error: any) {
+          console.log('Transaction Modify API Error:', error);
+     }
+});
