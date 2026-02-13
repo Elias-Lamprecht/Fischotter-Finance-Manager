9287f78946d190925926a53cbc9651d4f4a30e4e
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 23:16:48 +0100

    feat - API: add Delete API's for transactions.

---
+++ server/api/management/delete/all/transactions.delete.ts
@@ -0,0 +1,23 @@
+import { getFullAuthCookieContent } from '../../../../utils/getFullAuthCookieContent';
+import { db } from '../../../../database/client';
+import { transaction } from '../../../../database/schema/transaction';
+import { ERRORS } from '~~/server/utils/errors';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { state: 'denied', message: ERRORS.AUTH.INSUFFICIENT_PERMISSIONS };
+     }
+
+     try {
+          await db.delete(transaction);
+          return { state: 'success' };
+     } catch (error: any) {
+          console.log('Delete All Transactions API Error:', error);
+     }
+});
