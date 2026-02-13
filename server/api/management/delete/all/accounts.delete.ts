38490ef5c16ee0fde89af8e3e2cf98710e8edac5
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 20:03:50 +0100

    feat - API: create delete all API for Accounts

---
+++ server/api/management/delete/all/accounts.delete.ts
@@ -0,0 +1,24 @@
+import { getFullAuthCookieContent } from '../../../../utils/getFullAuthCookieContent';
+import { db } from '../../../../database/client';
+import { account } from '../../../../database/schema/account';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+
+     // TODO: Add the generalized Error Messages
+     if (FullAuthCookieContent === null) {
+          return { success: false, message: "User isn't logged in" };
+     }
+
+     // TODO: Add the generalized Error Messages
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { success: false, message: "User isn't a adminstrator" };
+     }
+     try {
+          await db.delete(account);
+          return { success: true };
+     } catch (error: any) {
+          console.log('Delete All Accounts API Error:', error);
+          return { success: false, error: error?.message ?? error };
+     }
+});
