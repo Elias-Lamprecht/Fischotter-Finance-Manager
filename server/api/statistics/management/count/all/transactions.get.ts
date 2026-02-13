df7f1eed644b9820f7568a980222b361c2ef68e8
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 23:18:22 +0100

    feat - API: add get-all and count api for Transactions.

---
+++ server/api/statistics/management/count/all/transactions.get.ts
@@ -0,0 +1,26 @@
+import { getFullAuthCookieContent } from '../../../../../utils/getFullAuthCookieContent';
+import { db } from '../../../../../database/client';
+import { transaction } from '../../../../../database/schema/transaction';
+import { count } from 'drizzle-orm';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { state: 'denied', message: ERRORS.AUTH.INSUFFICIENT_PERMISSIONS };
+     }
+
+     try {
+          const db_result = await db.select({ count: count() }).from(transaction);
+
+          const result = db_result[0]!.count;
+
+          return { state: 'success', data: result };
+     } catch (error: any) {
+          console.log('Transaction Count API Error:', error);
+     }
+});
