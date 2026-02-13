25fb3c083ae60e5b766c764291978f306e247b48
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 21:13:50 +0100

    addition - Errors: Added more types of errors.

--- server/utils/errors.ts
+++ server/utils/errors.ts
@@ -1,12 +1,19 @@
 export const ERRORS = {
      GENERAL: {
           MISSING_DATA: 'Missing Fields',
-          ERROR: 'A Error happened, please try later again',
+          ERROR: 'An unexpected error occurred. Please try again later.',
      },
      AUTH: {
-          NOT_FOUND: 'No User Account has been found with those crendentials',
+          USER_NOT_FOUND: 'No User Account has been found with those crendentials',
           NOT_ACTIVE: 'Your User Account hasnt been activated',
-          WRONG_PASSWORD: 'Invalid Credentials',
+          NOT_LOGGED_IN: 'Please login first before using the API',
+          INSUFFICIENT_PERMISSIONS:
+               'Your User Account is missing the needed permissions for this API',
+          INVALID_CREDENTIALS: 'Invalid Credentials',
           USERNAME_TAKEN: 'Username is already taken',
+     },
+     UPDATE: {
+          NO_CHANGES_DETECTED:
+               'No changes were detected. Please modify at least one field before updating.',
      },
 };
