d91e3ddeadb042e42a00643de282abf47edc6539
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-13 20:34:00 +0100

    feat - Errors: Add global Error list.

---
+++ server/utils/errors.ts
@@ -0,0 +1,12 @@
+export const ERRORS = {
+     GENERAL: {
+          MISSING_DATA: 'Missing Fields',
+          ERROR: 'A Error happened, please try later again',
+     },
+     AUTH: {
+          NOT_FOUND: 'No User Account has been found with those crendentials',
+          NOT_ACTIVE: 'Your User Account hasnt been activated',
+          WRONG_PASSWORD: 'Invalid Credentials',
+          USERNAME_TAKEN: 'Username is already taken',
+     },
+};
