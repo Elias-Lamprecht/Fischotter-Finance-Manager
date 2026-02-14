e7167131a805f7432f752a774e6a7011ed0fc270
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-14 12:44:51 +0100

    feat - API: Add get by id to Core.

---
+++ server/api/core/get/by-id/account.post.ts
@@ -0,0 +1,28 @@
+import { getFullAuthCookieContent } from '../../../../utils/getFullAuthCookieContent';
+import { db } from '../../../../database/client';
+import { account } from '../../../../database/schema/account';
+import { eq, and } from 'drizzle-orm';
+import { ERRORS } from '~~/server/utils/errors';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+     const body = await readBody(event);
+
+     if (!body.id) {
+          return { state: 'error', message: ERRORS.GENERAL.MISSING_DATA };
+     }
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     try {
+          const result = await db
+               .select()
+               .from(account)
+               .where(and(eq(body.id, account.id), eq(account.owner_id, FullAuthCookieContent.id)));
+          return { state: 'success', data: result };
+     } catch (error: any) {
+          console.log('Get by ID Account API Error:', error);
+     }
+});
---
+++ server/api/core/get/by-id/transaction.post.ts
@@ -0,0 +1,33 @@
+import { getFullAuthCookieContent } from '../../../../utils/getFullAuthCookieContent';
+import { db } from '../../../../database/client';
+import { transaction } from '../../../../database/schema/transaction';
+import { eq, and } from 'drizzle-orm';
+import { ERRORS } from '~~/server/utils/errors';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+     const body = await readBody(event);
+
+     if (!body.id) {
+          return { state: 'error', message: ERRORS.GENERAL.MISSING_DATA };
+     }
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     try {
+          const result = await db
+               .select()
+               .from(transaction)
+               .where(
+                    and(
+                         eq(body.id, transaction.id),
+                         eq(transaction.owner_id, FullAuthCookieContent.id),
+                    ),
+               );
+          return { state: 'success', data: result };
+     } catch (error: any) {
+          console.log('Get by ID Transaction API Error:', error);
+     }
+});
