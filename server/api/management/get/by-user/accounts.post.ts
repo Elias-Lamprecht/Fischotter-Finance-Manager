053396d9cb2d3aa0d625fb2197ca2c4b6836b24f
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-14 05:14:56 +0100

    feat - API: add API for getting all Accounts of user.

---
+++ server/api/management/get/by-user/accounts.post.ts
@@ -0,0 +1,32 @@
+import { getFullAuthCookieContent } from '../../../../utils/getFullAuthCookieContent';
+import { db } from '../../../../database/client';
+import { eq } from 'drizzle-orm';
+import { account } from '../../../../database/schema/account';
+import { ERRORS } from '~~/server/utils/errors';
+
+export default defineEventHandler(async (event) => {
+     const FullAuthCookieContent = getFullAuthCookieContent(event);
+     const body = await readBody(event);
+
+     if (!body.id) {
+          return { state: 'error', message: ERRORS.GENERAL.MISSING_DATA };
+     }
+
+     if (FullAuthCookieContent === null) {
+          return { state: 'denied', message: ERRORS.AUTH.NOT_LOGGED_IN };
+     }
+
+     if (FullAuthCookieContent.role !== 'admin') {
+          return { state: 'denied', message: ERRORS.AUTH.INSUFFICIENT_PERMISSIONS };
+     }
+
+     try {
+          const result = await db.select().from(account).where(eq(account.owner_id, body.id));
+
+          console.log(result);
+
+          return { state: 'success', data: result };
+     } catch (error: any) {
+          console.log('Get by User Accounts API Error:', error);
+     }
+});
