87a049e5d664fd23340fc2d80695ecbcdcad1516
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-16 00:28:31 +0100

    refactor - Composable: Update CreateNewAccount to update list after Creation.

--- app/composables/Accounts/useCreateNewAccount.ts
+++ app/composables/Accounts/useCreateNewAccount.ts
@@ -1,11 +1,11 @@
 import type { ApiResponse } from '@/types/API';
 import { ERRORS } from '#shared/utils/Errors';
-
-// TODO: Add Fetchall
+import { useFetchAllAccounts } from '@/composables/Accounts/useFetchAllAccounts';
 
 export function useCreateNewAccount() {
 	const error = ref('');
 	const loading = ref(false);
+	const { FetchAllAccounts } = useFetchAllAccounts();
 
 	async function CreateNewAccount(payload: {
 		owner_id: string;
@@ -25,7 +25,9 @@ export function useCreateNewAccount() {
 				},
 			});
 
-			if (response.state !== 'success') {
+			if (response.state === 'success') {
+				await FetchAllAccounts();
+			} else {
 				error.value = response.message || ERRORS.GENERAL.ERROR;
 			}
 		} catch (err) {
