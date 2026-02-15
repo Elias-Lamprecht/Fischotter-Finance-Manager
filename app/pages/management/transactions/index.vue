8897538d63d9c7f4bfae7b9dfc44512a7f8794cd
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-15 12:03:56 +0100

    refactor - Test Page: update Transaction Test Page to use Pageination & Add Selection.

--- app/pages/management/transactions/index.vue
+++ app/pages/management/transactions/index.vue
@@ -1,10 +1,4 @@
 <template>
-     <form @submit.prevent="DeleteAllTransactions()">
-          <button type="submit" :disabled="loading_delete_all_transactions">
-               {{ loading_delete_all_transactions ? 'Deleting...' : 'Delete all Transactions' }}
-          </button>
-     </form>
-
      <form @submit.prevent="CreateNewTransaction()">
           <br />
           <div>
@@ -49,9 +43,23 @@
 
      <p v-if="error" style="color: red">{{ error }}</p>
 
-     <p>Transactions Count: {{ TransactionsCount }}</p>
+     <form @submit.prevent="DeleteAllTransactions()">
+          <button type="submit" :disabled="loading_delete_all_transactions">
+               {{ loading_delete_all_transactions ? 'Deleting...' : 'Delete all Transactions' }}
+          </button>
+     </form>
 
-     <ul v-for="transaction in transactions" :key="transaction.id">
+     <form @submit.prevent="DeleteSelectedTransactions(SelectedTransactions)">
+          <button type="submit">Delete Selected Transactions</button>
+     </form>
+
+     <p>Total Transactions: {{ TotalTransactions }}</p>
+     <p>Selected Transactions: {{ SelectedTransactions.length }}</p>
+
+
+     <ul v-for="transaction in transactions" :key="transaction.id" style="display: flex;flex-direction: row;">
+          <li><input type="checkbox" :value="transaction.id" v-model="SelectedTransactions"></li>
+
           <li>ID: <input type="text" :value="transaction.id" disabled /></li>
 
           <li>Owner ID: <input type="text" v-model="transaction.owner_id" /></li>
@@ -98,14 +106,41 @@
                <button @click="UpdateTransaction(transaction)">Update Transaction</button>
           </li>
      </ul>
+     <ul>
+          <li>
+               <button @click="PreviousPage" :disabled="page === 1">
+                    Previous Page
+               </button>
+          </li>
+          <li>
+          <button @click="NextPage" :disabled="page === lastPage">
+               Next Page
+          </button>
+          </li>
+          <li>
+               <input
+                    type="number"
+                    v-model.number="pageInput"
+                    :min="1"
+                    :max="lastPage"
+               />
+               <button @click="GoToPage">
+                    Go
+               </button>
+          </li>
+          <li>
+               Last Page: {{ lastPage }}
+          </li>
+     </ul>
 </template>
 <script setup lang="ts">
 import { ERRORS } from '~~/server/utils/errors';
-import type { ApiResponse } from '@/types/API';
+import type { ApiResponse, PaginationApiResponse } from '@/types/API';
 import type { Transaction } from '@/types/Transaction';
 
 const transactions = ref<Transaction[]>([]);
-const TransactionsCount = ref<number>();
+const TotalTransactions = ref<number>();
+const SelectedTransactions = ref([]);
 
 const owner_id = ref('');
 const account_id = ref('');
@@ -114,42 +149,59 @@ const description = ref('');
 const type = ref('income');
 const price = ref(0);
 
+const page = ref(1);
+const limit = 10;
+const lastPage = ref(1);
+const pageInput = ref(1);
+
 const error = ref('');
 
 const loading_new_transaction = ref(false);
 const loading_delete_all_transactions = ref(false);
 
 onMounted(async () => {
-     await Promise.all([FetchAllTransactions(), FetchTransactionCount()]);
+     await Promise.all([FetchAllTransactions()]);
 });
 
-async function FetchAllTransactions() {
-     try {
-          const response = await $fetch<ApiResponse<Transaction[]>>('/api/management/get/all/transactions', {
-               method: 'GET',
-          });
-
-          if (response.state === 'success') {
-               transactions.value = response.data || [];
-          } else {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
+async function NextPage() {
+     if (page.value < lastPage.value) {
+          page.value++;
+          await FetchAllTransactions();
      }
 }
 
-async function FetchTransactionCount() {
+async function PreviousPage() {
+     if (page.value > 1) {
+          page.value--;
+          await FetchAllTransactions();
+     }
+}
+
+async function GoToPage() {
+     if (pageInput.value < 1) return;
+     if (pageInput.value > lastPage.value) return;
+
+     page.value = pageInput.value;
+     await FetchAllTransactions();
+}
+
+async function FetchAllTransactions() {
      try {
-          const response = await $fetch<ApiResponse<number>>('/api/statistics/management/count/all/transactions', {
-               method: 'GET',
+          const response = await $fetch<PaginationApiResponse<Transaction[]>>('/api/management/get/all/transactions', {
+               method: 'POST',
+               body: {
+                    page: page.value,
+                    limit: limit
+               }
           });
 
-          if (response.state === 'success') {
-               TransactionsCount.value = response.data || 0;
-          } else {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
+     if (response.state === 'success') {
+          transactions.value = response.data;
+          TotalTransactions.value = response.pagination.total;
+          lastPage.value = response.pagination.lastPage;
+     } else if (response.state === 'denied' || response.state === 'error') {
+          error.value = response.message || ERRORS.GENERAL.ERROR;
+     }
      } catch (err) {
           error.value = ERRORS.GENERAL.ERROR;
      }
@@ -182,7 +234,7 @@ async function CreateNewTransaction() {
           error.value = ERRORS.GENERAL.ERROR;
      } finally {
           loading_new_transaction.value = false;
-          await Promise.all([FetchAllTransactions(), FetchTransactionCount()]);
+          await Promise.all([FetchAllTransactions()]);
      }
 }
 
@@ -226,7 +278,7 @@ async function DeleteTransaction(id: string) {
      } catch (err) {
           error.value = ERRORS.GENERAL.ERROR;
      } finally {
-          await Promise.all([FetchAllTransactions(), FetchTransactionCount()]);
+          await Promise.all([FetchAllTransactions()]);
      }
 }
 
@@ -234,7 +286,7 @@ async function DeleteAllTransactions() {
      loading_delete_all_transactions.value = true;
      error.value = '';
 
-     try {
+          try {
           const response = await $fetch<ApiResponse>('/api/management/delete/all/transactions', {
                method: 'DELETE',
           });
@@ -245,9 +297,28 @@ async function DeleteAllTransactions() {
      } catch (err) {
           error.value = ERRORS.GENERAL.ERROR;
      } finally {
-          loading_delete_all_transactions.value = false;
-          await Promise.all([FetchAllTransactions(), FetchTransactionCount()]);
+          await FetchAllTransactions();
      }
 }
 
+async function DeleteSelectedTransactions(TransactionArray: any) {
+          try {
+          const response = await $fetch<ApiResponse>('/api/management/delete/many/transactions', {
+               method: 'DELETE',
+               body: {
+                    TransactionArray: TransactionArray
+               },
+          });
+
+          if (response.state === 'success') {
+               SelectedTransactions.value.length = 0;
+          } else {
+               error.value = response.message || ERRORS.GENERAL.ERROR;
+          }
+     } catch (err) {
+          error.value = ERRORS.GENERAL.ERROR;
+     } finally {
+          await FetchAllTransactions();
+     }
+}
 </script>
