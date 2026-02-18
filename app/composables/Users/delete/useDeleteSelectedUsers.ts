1e1f28bbacc3bbe445db055af67644a4a9fa7cd0
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-18 20:07:21 +0100

    feat - Composable: create useDeleteSelectedUsers Composable.

---
+++ app/composables/Users/delete/useDeleteSelectedUsers.ts
@@ -0,0 +1,39 @@
+import type { ApiResponse } from '@/types/API';
+import { ERRORS } from '#shared/utils/Errors';
+import { useFetchPagenizedUsers } from '@/composables/Users/useFetchPagenizedUsers';
+import { useConfirm } from '@/composables/useConfirm';
+
+export function useDeleteSelectedUsers() {
+	const error = ref('');
+	const SelectedUsers = ref([]);
+	const { FetchPagenizedUsers } = useFetchPagenizedUsers();
+	const { showConfirm } = useConfirm();
+
+	async function DeleteSelectedUsers(UserArray: any) {
+		const result = await showConfirm('Are you sure you want to delete the selected Users?');
+
+		if (result) {
+			try {
+				const response = await $fetch<ApiResponse>(
+					'/api/management/delete/many/users',
+					{
+						method: 'DELETE',
+						body: {
+							UserArray: UserArray,
+						},
+					},
+				);
+
+				if (response.state === 'success') {
+					await FetchPagenizedUsers();
+					SelectedUsers.value.length = 0;
+				} else {
+					error.value = response.message || ERRORS.GENERAL.ERROR;
+				}
+			} catch (err) {
+				error.value = ERRORS.GENERAL.ERROR;
+			}
+		}
+	}
+	return { error, SelectedUsers, DeleteSelectedUsers };
+}
