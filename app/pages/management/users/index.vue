10c6ffd83cd62b7c8f68241e9c1b16009dabcd05
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-18 20:08:14 +0100

    refactor - User Management Page: refactor it to match transactions and accounts.

--- app/pages/management/users/index.vue
+++ app/pages/management/users/index.vue
@@ -1,282 +1,133 @@
 <template>
-     <form @submit.prevent="DeleteAllUsers()">
-          <button type="submit" :disabled="loading_delete_all_users">
-               {{ loading_delete_all_users ? 'Deleting...' : 'Delete all Users' }}
-          </button>
-     </form>
+	<DeleteAllUsersForm />
+	<CreateNewUserForm />
 
-     <form @submit.prevent="CreateNewUser()">
-          <br />
-          <div>
-               <label for="username">Username:</label>
-               <input v-model="username" id="username" type="text" required />
-          </div>
+	<form @submit.prevent="DeleteSelectedUsers(SelectedUsers)">
+		<button type="submit">Delete Selected Users</button>
+	</form>
 
-          <div>
-               <label for="email">Email:</label>
-               <input v-model="email" id="email" type="text" />
-          </div>
+	<p>Total Users: {{ TotalUsers }}</p>
+	<p>Selected Users: {{ SelectedUsers.length }}</p>
 
-          <div>
-               <label for="password">Password:</label>
-               <input v-model="password" id="password" type="text" required />
-          </div>
-          <br />
-          <button type="submit" :disabled="loading_new_user">
-               {{ loading_new_user ? 'Creating...' : 'Create new User' }}
-          </button>
-          <br />
-     </form>
+	<ul v-for="user in users" :key="user.id" style="display: flex; flex-direction: row">
+		<li><input type="checkbox" :value="user.id" v-model="SelectedUsers" /></li>
 
-     <p v-if="error" style="color: red">{{ error }}</p>
+		<li>ID: <input type="text" :value="user.id" disabled /></li>
 
-     <p>User Count: {{ UserCount }}</p>
+		<li v-if="user.email">Email: <input type="text" v-model="user.email" /></li>
 
-     <ul v-for="user in users" :key="user.id">
-          <li>ID: <input type="text" :value="user.id" disabled /></li>
+		<li>Username: <input type="text" v-model="user.username" /></li>
 
-          <li v-if="user.email">Email: <input type="text" v-model="user.email" /></li>
+		<li>Displayname: <input type="text" v-model="user.displayname" /></li>
 
-          <li>Username: <input type="text" v-model="user.username" /></li>
+		<li>
+			Role:
+			<select v-model="user.role">
+				<option value="user">user</option>
+				<option value="admin">admin</option>
+			</select>
+		</li>
 
-          <li>Displayname: <input type="text" v-model="user.displayname" /></li>
+		<li>
+			Status:
+			<select v-model="user.status">
+				<option value="active">active</option>
+				<option value="disabled">disabled</option>
+			</select>
+		</li>
 
-          <li>
-               Role:
-               <select v-model="user.role">
-                    <option value="user">user</option>
-                    <option value="admin">admin</option>
-               </select>
-          </li>
+		<li>
+			Created at:
+			{{
+				new Date(user.created_at).toLocaleString('en-UK', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric',
+					hour: '2-digit',
+					minute: '2-digit',
+				})
+			}}
+		</li>
 
-          <li>
-               Status:
-               <select v-model="user.status">
-                    <option value="active">active</option>
-                    <option value="disabled">disabled</option>
-               </select>
-          </li>
+		<li>
+			<button @click="DeleteUser(user.id)">Delete User</button>
+		</li>
 
-          <li>
-               <button @click="selectUser(user)">Load Accounts</button>
-          </li>
-
-          <li v-if="selectedUserId === user.id">
-               <p v-if="loadingUserData">Loading details...</p>
-               <div v-else>
-                    <p v-for="account in selectedUserData" :key="account.id" >
-                         <ul>ID: {{ account.id }}</ul>
-                         <ul>Title: {{ account.title }}</ul>
-                         <ul>Description: {{ account.description }}</ul>
-                         <ul>Created at: {{ new Date(account.created_at).toLocaleString('en-UK', {
-                         year: 'numeric',
-                         month: 'long',
-                         day: 'numeric',
-                         hour: '2-digit',
-                         minute: '2-digit',
-                    }) }}
-                         </ul>
-                    </p>
-               </div>
-          </li>
-
-          <li>
-               Created at:
-               {{
-                    new Date(user.created_at).toLocaleString('en-UK', {
-                         year: 'numeric',
-                         month: 'long',
-                         day: 'numeric',
-                         hour: '2-digit',
-                         minute: '2-digit',
-                    })
-               }}
-          </li>
-
-          <li>
-               <button @click="DeleteUser(user.id)">Delete User</button>
-          </li>
-
-          <li>
-               <button @click="UpdateUser(user)">Update User</button>
-          </li>
-          <br />
-     </ul>
+		<li>
+			<button @click="UpdateUser(user)">Update User</button>
+		</li>
+		<br />
+	</ul>
+	<ul>
+		<li>
+			<button @click="PreviousPage" :disabled="page === 1">Previous Page</button>
+		</li>
+		<li>
+			<button @click="NextPage" :disabled="page === lastPage">Next Page</button>
+		</li>
+		<li>
+			<input type="number" v-model.number="pageInput" :min="1" :max="lastPage" />
+			<button @click="GoToPage">Go</button>
+		</li>
+		<li>Last Page: {{ lastPage }}</li>
+	</ul>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { ERRORS } from '#shared/utils/Errors';
-import type { ApiResponse } from '@/types/API';
-import type { User } from '@/types/User';
-import type { Account } from '@/types/Account';
+// COMPOSABLES
+import { useFetchPagenizedUsers } from '@/composables/Users/useFetchPagenizedUsers';
+import { useDeleteUser } from '@/composables/Users/delete/useDeleteUser';
+import { useDeleteSelectedUsers } from '@/composables/Users/delete/useDeleteSelectedUsers';
+import { useUpdateUser } from '@/composables/Users/useUpdateUser';
 
-const users = ref<User[]>([]);
-const UserCount = ref<number>(0);
+// COMPONENTS
+import DeleteAllUsersForm from '@/components/management/users/DeleteAllUsersForm.vue';
+import CreateNewUserForm from '@/components/management/users/CreateNewUserForm.vue';
 
-const username = ref('');
-const email = ref('');
-const password = ref('');
+const {
+	users,
+	TotalUsers,
+	lastPage,
+	error: fetchError,
+	page,
+	FetchPagenizedUsers,
+} = useFetchPagenizedUsers();
 
-const selectedUserId = ref<string | null>(null);
-const selectedUserData = ref<Account[]>([]);
-const loadingUserData = ref(false);
+const { error: deleteUserError, DeleteUser } = useDeleteUser();
 
-const error = ref('');
+const {
+	error: deleteSelectedUsersError,
+	SelectedUsers,
+	DeleteSelectedUsers,
+} = useDeleteSelectedUsers();
 
-const loading_new_user = ref(false);
-const loading_delete_user = ref(false);
-const loading_delete_all_users = ref(false);
+const { error: updateUserError, UpdateUser } = useUpdateUser();
 
-onMounted(async () => {
-     await Promise.all([FetchAllUsers(), FetchUserCount()]);
-});
+const pageInput = ref(1);
 
-async function FetchAllUsers() {
-     try {
-          const response = await $fetch<ApiResponse<User[]>>('/api/management/get/all/users', {
-               method: 'GET',
-          });
+onMounted(() => FetchPagenizedUsers());
 
-          if (response.state === 'success') {
-               users.value = response.data || [];
-          } else {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     }
+async function NextPage() {
+	if (page.value < lastPage.value) {
+		page.value++;
+		pageInput.value = page.value;
+		await FetchPagenizedUsers();
+	}
 }
 
-async function FetchUserCount() {
-     try {
-          const response = await $fetch<ApiResponse<number>>(
-               '/api/statistics/management/count/all/users',
-               { method: 'GET' },
-          );
-          if (response.state === 'success') {
-               UserCount.value = response.data || 0;
-          } else {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     }
+async function PreviousPage() {
+	if (page.value > 1) {
+		page.value--;
+		pageInput.value = page.value;
+		await FetchPagenizedUsers();
+	}
 }
 
-async function selectUser(user: User) {
-     selectedUserId.value = user.id;
-     selectedUserData.value = [];
-     error.value = '';
-     loadingUserData.value = true;
+async function GoToPage() {
+	if (pageInput.value < 1) return;
+	if (pageInput.value > lastPage.value) return;
 
-     try {
-          const response = await $fetch<ApiResponse<Account[]>>(
-               '/api/management/get/by-user/accounts',
-               {
-                    method: 'POST',
-                    body: { id: user.id },
-               },
-          );
-
-          if (response.state === 'success') {
-               selectedUserData.value = response.data || [];
-          } else {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     } finally {
-          loadingUserData.value = false;
-     }
-}
-
-async function CreateNewUser() {
-     loading_new_user.value = true;
-     error.value = '';
-
-     try {
-          const response = await $fetch<ApiResponse>('/api/public/register', {
-               method: 'POST',
-               body: { username: username.value, email: email.value, password: password.value },
-          });
-
-          if (response.state === 'success') {
-               username.value = '';
-               email.value = '';
-               password.value = '';
-          } else {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     } finally {
-          loading_new_user.value = false;
-          await Promise.all([FetchAllUsers(), FetchUserCount()]);
-     }
-}
-
-async function UpdateUser(user: User) {
-     try {
-          const response = await $fetch<ApiResponse>('/api/management/modify/user', {
-               method: 'POST',
-               body: {
-                    id: user.id,
-                    username: user.username,
-                    displayname: user.displayname,
-                    email: user.email,
-                    role: user.role,
-                    status: user.status,
-                },
-          });
-
-          if (response.state !== 'success') {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     } finally {
-          await FetchAllUsers();
-     }
-}
-
-async function DeleteUser(id: string) {
-     loading_delete_user.value = true;
-     try {
-          const response = await $fetch<ApiResponse>('/api/management/delete/by-id/user', {
-               method: 'DELETE',
-               body: { id: id },
-          });
-
-          if (response.state !== 'success') {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     } finally {
-          loading_delete_user.value = false;
-          await Promise.all([FetchAllUsers(), FetchUserCount()]);
-     }
-}
-
-async function DeleteAllUsers() {
-     loading_delete_all_users.value = true;
-     error.value = '';
-
-     try {
-          const response = await $fetch<ApiResponse>('/api/management/delete/all/users', {
-               method: 'DELETE',
-          });
-
-          if (response.state !== 'success') {
-               error.value = response.message || ERRORS.GENERAL.ERROR;
-
-          }
-     } catch (err) {
-          error.value = ERRORS.GENERAL.ERROR;
-     } finally {
-          loading_delete_all_users.value = false;
-          await Promise.all([FetchAllUsers(), FetchUserCount()]);
-     }
+	page.value = pageInput.value;
+	await FetchPagenizedUsers();
 }
 </script>
