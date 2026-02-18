f07d8afd96626d807c393c3979c55cc7c6ad5234
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-18 20:07:51 +0100

    feat - Component: create DeleteAllUsersForm Component.

---
+++ app/components/management/users/DeleteAllUsersForm.vue
@@ -0,0 +1,13 @@
+<template>
+	<form @submit.prevent="DeleteAllUsers()">
+		<button type="submit" :disabled="loading">
+			{{ loading ? 'Deleting...' : 'Delete all Users' }}
+		</button>
+		{{ error }}
+	</form>
+</template>
+<script setup lang="ts">
+import { useDeleteAllUsers } from '~/composables/Users/delete/useDeleteAllUsers';
+
+const { error, loading, DeleteAllUsers } = useDeleteAllUsers();
+</script>
