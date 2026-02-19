620969bcebf4b7c26f9bcbe2720edb4eee1375e3
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-19 23:16:17 +0100

    refactor - Account Page: refactor Account Page to look decent.

--- app/pages/management/accounts/index.vue
+++ app/pages/management/accounts/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<h2>Account Management</h2>
+	<h2 class="management__page-title">Account Management</h2>
 
 	<div class="management__page-header">
 		<button @click="showModal = true" class="management__button">Create Account</button>
@@ -12,56 +12,79 @@
 			<DeleteAllAccountsForm />
 		</div>
 	</div>
-	<br />
-	<p>Total Accounts: {{ TotalAccounts }}</p>
-	<p>Selected Accounts: {{ SelectedAccounts.length }}</p>
+	<div class="management__count-page-wrapper">
+		<div class="management__count-wrapper">
+			<p>Total Accounts: {{ TotalAccounts }}</p>
+			<p>Selected Accounts: {{ SelectedAccounts.length }}</p>
+		</div>
+	</div>
 
-	<ul v-for="account in accounts" :key="account.id" style="display: flex; flex-direction: row">
-		<li><input type="checkbox" :value="account.id" v-model="SelectedAccounts" /></li>
+	<div class="EntityList">
+		<div class="EntityList__grid">
+			<div class="EntityList__row">
+				<div class="EntityList__header"><b>Select</b></div>
+				<div class="EntityList__header"><b>ID</b></div>
+				<div class="EntityList__header"><b>Owner ID</b></div>
+				<div class="EntityList__header"><b>Title</b></div>
+				<div class="EntityList__header"><b>Description</b></div>
+				<div class="EntityList__header"><b>Created At</b></div>
+				<div class="EntityList__header"><b>Actions</b></div>
+			</div>
 
-		<li>ID: <input type="text" :value="account.id" disabled /></li>
-
-		<li>Owner ID: <input type="text" v-model="account.owner_id" /></li>
-
-		<li>Title: <input type="text" v-model="account.title" /></li>
-
-		<li v-if="account.description">
-			Description: <textarea type="text" v-model="account.description"></textarea>
-		</li>
-
-		<li>
-			Created at:
-			{{
-				new Date(account.created_at).toLocaleString('en-UK', {
-					year: 'numeric',
-					month: 'long',
-					day: 'numeric',
-					hour: '2-digit',
-					minute: '2-digit',
-				})
-			}}
-		</li>
-
-		<li>
-			<button
-				@click="UpdateAccount(account)"
-				class="management__button management__update-button"
+			<div
+				v-for="account in accounts"
+				:key="account.id"
+				class="EntityList__row"
+				:class="{ selected: SelectedAccounts.includes(account.id) }"
 			>
-				Update Account
-			</button>
-		</li>
-		<li>
-			<button
-				@click="DeleteAccount(account.id)"
-				class="management__button management__delete-button"
-			>
-				Delete Account
-			</button>
-		</li>
-		<br />
-	</ul>
+				<!-- Checkbox -->
+				<div class="EntityList__list-item EntityList__list-checkbox-item">
+					<input type="checkbox" :value="account.id" v-model="SelectedAccounts" />
+				</div>
+				<div class="EntityList__list-item">
+					<input type="text" :value="account.id" disabled />
+				</div>
+				<div class="EntityList__list-item">
+					<input type="text" v-model="account.owner_id" />
+				</div>
+				<div class="EntityList__list-item">
+					<input type="text" v-model="account.title" />
+				</div>
+				<div class="EntityList__list-item">
+					<textarea v-model="account.description"></textarea>
+				</div>
+				<div class="EntityList__list-item EntityList__list-date-item">
+					{{
+						new Date(account.created_at).toLocaleString('en-UK', {
+							year: 'numeric',
+							month: 'long',
+							day: 'numeric',
+							hour: '2-digit',
+							minute: '2-digit',
+						})
+					}}
+				</div>
+				<!-- Actions -->
+				<div class="EntityList__actions EntityList__list-item">
+					<button
+						@click="UpdateAccount(account)"
+						class="management__button management__update-button"
+					>
+						Update
+					</button>
+					<button
+						@click="DeleteAccount(account.id)"
+						class="management__button management__delete-button"
+					>
+						Delete
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
 	<CreateNewAccountForm v-model:show="showModal" />
 </template>
+
 <style src="@/assets/management/index.scss" lang="scss"></style>
 <script setup lang="ts">
 // COMPONENTS
