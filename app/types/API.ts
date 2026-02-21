2055aef70b9dcbea354dcf180560931369991899
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-21 21:15:39 +0100

    refactor - Type Names: refactor type names to be more useful.

--- app/types/API.ts
+++ app/types/API.ts
@@ -1,23 +1,23 @@
 export interface ApiResponse<T = any> {
-     state: 'success' | 'error' | 'denied';
-     data?: T;
-     message?: string;
+	state: 'success' | 'error' | 'denied';
+	data?: T;
+	message?: string;
 }
 
-export type PaginationApiResponse<T> =
-     | {
-            state: 'success';
-            data: T;
-            pagination: {
-                 total: number;
-                 page: number;
-                 limit: number;
-                 lastPage: number;
-                 hasNext: boolean;
-                 hasPrev: boolean;
-            };
-       }
-     | {
-            state: 'denied' | 'error';
-            message: string;
-       };
+export type PaginatedResponse<T> =
+	| {
+			state: 'success';
+			data: T;
+			pagination: {
+				total: number;
+				page: number;
+				limit: number;
+				lastPage: number;
+				hasNext: boolean;
+				hasPrev: boolean;
+			};
+	  }
+	| {
+			state: 'denied' | 'error';
+			message: string;
+	  };
