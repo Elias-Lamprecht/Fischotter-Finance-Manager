a51db9a5fd29b8d770d0dfea4e5e88d290c0e445
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-21 21:54:21 +0100

    Move useConfirm Composable File Location to be more clear what it is.

--- app/components/utils/ConfirmModal.vue
+++ app/components/utils/ConfirmModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useConfirm } from '@/composables/useConfirm';
+import { useConfirm } from '~/composables/utils/useConfirm';
 
 const { isOpen, message, answer } = useConfirm();
 </script>
