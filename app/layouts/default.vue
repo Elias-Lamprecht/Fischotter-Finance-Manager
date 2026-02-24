80e86331484a82c3be5e962366a917611f6c4842
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-24 18:23:10 +0100

    Update Default Layout to use Sidebar instead of header.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,25 @@
 <template>
-	<Header />
+	<div class="page">
+		<Sidebar />
+		<NuxtPage />
+	</div>
 	<ConfirmModal />
-	<NuxtPage />
 </template>
-
+<style>
+	.page {
+		display: flex;
+	}
+</style>
 <script setup>
-import Header from '@/components/global/header.vue';
-import ConfirmModal from '@/components/utils/ConfirmModal.vue';
+	import Sidebar from '@/components/global/sidebar.vue';
+	import ConfirmModal from '@/components/utils/ConfirmModal.vue';
+
+	useHead({
+		link: [
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400&display=swap',
+			},
+		],
+	});
 </script>
