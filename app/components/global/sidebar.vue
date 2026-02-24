0cc167cac4b924d38290de49246bd89367a5d9a6
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-24 18:23:45 +0100

    Create Sidebar.

---
+++ app/components/global/sidebar.vue
@@ -0,0 +1,141 @@
+<template>
+	<nav class="sidebar">
+		<div class="sidebar__section">
+			<p class="sidebar__heading">User Space</p>
+			<div class="sidebar__item">
+				<div class="sidebar__item-header">
+					<Home
+						class="sidebar__icon"
+						:size="18"
+					/>
+					<a
+						href="#"
+						class="sidebar__link"
+						>Home</a
+					>
+				</div>
+			</div>
+			<div class="sidebar__item">
+				<div class="sidebar__item-header">
+					<LayoutDashboard
+						class="sidebar__icon"
+						:size="18"
+					/>
+					<a
+						href="#"
+						class="sidebar__link"
+						>Dashboard</a
+					>
+					<ChevronDown
+						@click="SwitchUserDashboard"
+						v-if="UserDashboard"
+						class="sidebar__chevron"
+					/>
+					<ChevronUp
+						@click="SwitchUserDashboard"
+						v-if="!UserDashboard"
+						class="sidebar__chevron"
+					/>
+				</div>
+
+				<ul
+					v-if="UserDashboard"
+					class="sidebar__submenu"
+				>
+					<li class="sidebar__submenu-item">Accounts</li>
+					<li class="sidebar__submenu-item">Transactions</li>
+				</ul>
+			</div>
+			<div class="sidebar__item">
+				<div class="sidebar__item-header">
+					<Settings
+						class="sidebar__icon"
+						:size="18"
+					/>
+					<a
+						href="#"
+						class="sidebar__link"
+						>Settings</a
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="sidebar__section">
+			<p class="sidebar__heading">Management Space</p>
+			<div class="sidebar__item">
+				<div class="sidebar__item-header">
+					<LayoutDashboard
+						class="sidebar__icon"
+						:size="18"
+					/>
+					<a
+						href="#"
+						class="sidebar__link"
+						>Dashboard</a
+					>
+					<ChevronDown
+						@click="SwitchAdminDashboard"
+						v-if="AdminDashboard"
+						class="sidebar__chevron"
+					/>
+					<ChevronUp
+						@click="SwitchAdminDashboard"
+						v-if="!AdminDashboard"
+						class="sidebar__chevron"
+					/>
+				</div>
+
+				<ul
+					v-if="AdminDashboard"
+					class="sidebar__submenu"
+				>
+					<li class="sidebar__submenu-item">
+						<NuxtLink to="/management/users">Users</NuxtLink>
+					</li>
+					<li class="sidebar__submenu-item">
+						<NuxtLink to="/management/accounts">Accounts</NuxtLink>
+					</li>
+					<li class="sidebar__submenu-item">
+						<NuxtLink to="/management/transactions">Transactions</NuxtLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="sidebar__section sidebar__bottom">
+			<div class="sidebar__user-profile">
+				<p
+					class="sidebar__user-profile-displayname"
+					v-if="LoggedInUserStore.UserData.displayname"
+				>
+					{{ LoggedInUserStore.UserData.displayname }}
+				</p>
+				<p
+					class="sidebar__user-profile-username"
+					v-if="LoggedInUserStore.UserData.username"
+				>
+					@{{ LoggedInUserStore.UserData.username }}
+				</p>
+			</div>
+		</div>
+	</nav>
+</template>
+<style src="@/assets/global/sidebar.scss" lang="scss"></style>
+<script setup lang="ts">
+	import { Home, LayoutDashboard, ChevronDown, ChevronUp, Settings } from 'lucide-vue-next';
+	import { useLoggedInUserStore } from '@/stores/LoggedInUser';
+
+	const LoggedInUserStore = useLoggedInUserStore();
+
+	const UserDashboard = ref(true);
+	const AdminDashboard = ref(true);
+
+	function SwitchAdminDashboard() {
+		AdminDashboard.value = !AdminDashboard.value;
+	}
+
+	function SwitchUserDashboard() {
+		UserDashboard.value = !UserDashboard.value;
+	}
+</script>
