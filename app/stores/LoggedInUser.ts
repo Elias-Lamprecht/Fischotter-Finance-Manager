5bbf886e49c7f7e579f46d1b61c35cc5684da0cc
Elias Lamprecht <eliaslamprecht3@gmail.com>
2026-02-24 18:22:21 +0100

    Create Logged In User Store.

---
+++ app/stores/LoggedInUser.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+
+export const useLoggedInUserStore = defineStore('LoggedInUser', {
+	state: () => ({
+		UserData: {} as {
+			username?: string;
+			displayname?: string;
+			email?: string;
+		},
+	}),
+
+	actions: {
+		async SetLoggedInUserData(payload: {
+			username?: string;
+			displayname?: string;
+			email?: string;
+		}) {
+			this.UserData = {
+				username: payload.username,
+				displayname: payload.displayname,
+				email: payload.email,
+			};
+		},
+	},
+});
